1dd4605a69045e91f70e0ad53c539883011a7383
Lukas Nagel <nagellukas04@outlook.com>
2021-12-03 12:25:51 +0100

    added sensor_client.py

---
+++ sensor_client.py
@@ -0,0 +1,29 @@
+import self as self
+
+
+class SensorClient:
+    self.Room = ""
+    self.BrokerIp = ""
+    self.Interval = ""
+    self.Temperature = ""
+    self.Humidity = ""
+    self.TemperatureLimit = ""
+    self.HumidityLimit = ""
+
+    def __init__(self):
+        print("Not implemented")
+
+    def ReadDataFromSensor(self):
+        print("Not implemented")
+
+    def StatusInfo(self):
+        print("Not implemented")
+
+    def Run(self):
+        print("Not implemented")
+
+    def MessageReceived(self):
+        print("Not implemented")
+
+    def SaveLimits(self):
+        print("Not implemented")
